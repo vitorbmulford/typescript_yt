7f10a1b99b862a94aea34fb46434f1aa874ea9c1
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2024-11-25 20:20:08 -0300

    aula 05 union types

---
+++ build/aula05.js
@@ -0,0 +1,10 @@
+"use strict";
+let vteste;
+vteste = "Bruno";
+vteste = 1233;
+let cursos = ["Javzascript", "Typescript", "C++"];
+let valores = [15, 23, 5, 3];
+cursos.push("Arduino");
+valores.push(5000);
+console.table(cursos);
+console.log(vteste);
---
+++ src/aula05.ts
@@ -0,0 +1,11 @@
+// Union Types. usar com cautela
+
+let vteste: string[] | string | number;
+vteste = "Bruno";
+vteste = 1233;
+let cursos: string[] = ["Javzascript", "Typescript", "C++"];
+let valores: number[] = [15, 23, 5, 3];
+cursos.push("Arduino");
+valores.push(5000);
+console.table(cursos);
+console.log(vteste);
