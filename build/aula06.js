5510da2afdfa9e7b32dcc0db9fdf8aa900b6e591
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2024-11-26 17:15:50 -0300

    aula 06 - array readonly

--- build/aula06.js
+++ build/aula06.js
@@ -3,3 +3,6 @@ let vnumeros = [20, 30, 40, []];
 let numeros = [20, 30, 40, []];
 let bnumeros = [20, 30, 40];
 numeros.push(20);
+numeros.unshift(10);
+console.log(numeros);
+let numeros_ro = [1, 2, 3, 4];
--- src/aula06.ts
+++ src/aula06.ts
@@ -2,5 +2,8 @@
 let vnumeros: (number | number[])[] = [20, 30, 40, []];
 let numeros: Array<number | number[]> = [20, 30, 40, []];
 let bnumeros: number[] = [20, 30, 40];
-
 numeros.push(20);
+numeros.unshift(10);
+console.log(numeros);
+
+let numeros_ro: ReadonlyArray<number> = [1, 2, 3, 4];
