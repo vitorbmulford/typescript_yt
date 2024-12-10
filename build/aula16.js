6e83c14e6c3231e934ccd810d71fb84aee457d5d
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2024-12-09 23:24:55 -0300

    aula 16

---
+++ build/aula16.js
@@ -0,0 +1,10 @@
+"use strict";
+class Comptuador {
+    nome;
+    ram;
+    cpu;
+    ligado;
+}
+const comp1 = new Comptuador();
+const comp2 = new Comptuador();
+const comp3 = new Comptuador();
---
+++ src/aula16.ts
@@ -0,0 +1,9 @@
+class Comptuador {
+  nome: string;
+  ram: number;
+  cpu: number;
+  ligado: boolean;
+}
+const comp1 = new Comptuador();
+const comp2 = new Comptuador();
+const comp3 = new Comptuador();
