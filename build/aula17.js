c4a586fc0fa230d812fe966182d9ec2a14a134ca
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2025-01-06 19:29:08 -0300

    aula 17, construtor e metodos

--- build/aula17.js
+++ build/aula17.js
@@ -11,8 +11,18 @@ class Comptuador2 {
         this.ligado = false;
         console.log("novo computado criado");
     }
+    info() {
+        console.log(`Nome...: ${this.nome}`);
+        console.log(`Ram....: ${this.ram}`);
+        console.log(`Cpu....: ${this.cpu}`);
+        console.log(`Ligado.: ${this.ligado ? "Sim" : "Não"}`);
+        console.log("-------------------------");
+    }
 }
 const comp12 = new Comptuador2("rapidao", 16, 4);
 const comp22 = new Comptuador2("carao", 8, 2);
 const comp32 = new Comptuador2("gamer", 32, 8);
-console.log(comp12, comp22, comp32);
+comp12.ligado = true;
+comp12.info();
+comp22.info();
+comp32.info();
