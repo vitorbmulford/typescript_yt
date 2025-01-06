20aad7a92aba0c7d9bc45b6d7cd098709a68a61e
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2025-01-06 19:03:53 -0300

    aula 16 oop

--- build/aula16.js
+++ build/aula16.js
@@ -1,10 +1,14 @@
 "use strict";
 class Comptuador {
-    nome;
-    ram;
-    cpu;
-    ligado;
+    nome = "comp1";
+    ram = 0;
+    cpu = 0;
+    ligado = false;
 }
 const comp1 = new Comptuador();
 const comp2 = new Comptuador();
 const comp3 = new Comptuador();
+comp1.nome = "Rapidao";
+comp2.nome = "Carao";
+comp3.nome = "Gamer";
+console.log(comp1, comp2, comp3);
--- src/aula16.ts
+++ src/aula16.ts
@@ -1,9 +1,16 @@
 class Comptuador {
-  nome: string;
-  ram: number;
-  cpu: number;
-  ligado: boolean;
+  nome: string = "comp1";
+  ram: number = 0;
+  cpu: number = 0;
+  ligado: boolean = false;
 }
+
 const comp1 = new Comptuador();
 const comp2 = new Comptuador();
 const comp3 = new Comptuador();
+
+comp1.nome = "Rapidao";
+comp2.nome = "Carao";
+comp3.nome = "Gamer";
+
+console.log(comp1, comp2, comp3);
